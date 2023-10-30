0eb28af2a6253a74f0adf2d9022da410bfc59c1b
Jawad <jawadrahman483@gmail.com>
2023-10-31 02:36:57 +0600

    31update

--- Frontend/src/components/BuildPlanMyself.js
+++ Frontend/src/components/BuildPlanMyself.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import loadGoogleMapsScript from '../pages/googleMaps';
+
 import './BuildPlanMyself.css';
 
 const BuildPlanMyself = () => {
+  const [placesText, setPlacesText] = useState('');
+  const [placesTextTo, setPlacesTextTo] = useState('');
+  const autocompleteInputRef = useRef(null);
+  const autocompleteInputToRef = useRef(null);
+
+  useEffect(() => {
+    loadGoogleMapsScript(() => {
+      if (window.google) {
+        const autocomplete = new window.google.maps.places.Autocomplete(autocompleteInputRef.current, {
+          componentRestrictions: { country: 'BD' },
+        });
+
+        const autocompleteTo = new window.google.maps.places.Autocomplete(autocompleteInputToRef.current, {
+          componentRestrictions: { country: 'BD' },
+        });
+
+        autocomplete.addListener('place_changed', function () {
+          const place = autocomplete.getPlace();
+          console.log(place);
+        });
+
+        autocompleteTo.addListener('place_changed', function () {
+          const placeTo = autocompleteTo.getPlace();
+          console.log(placeTo);
+        });
+      }
+    });
+  }, []);
+
+  const handlePlacesTextChange = (event) => {
+    setPlacesText(event.target.value);
+  };
+
+  const handlePlacesTextChangeTo = (event) => {
+    setPlacesTextTo(event.target.value);
+  };
+
   return (
     <div className="build-plan-myself">
       <div className="upper-div">
-        <input className="from" type="text" placeholder="From" />
+        <input
+          className="from"
+          type="text"
+          placeholder="From"
+          value={placesText}
+          onChange={handlePlacesTextChange}
+          ref={autocompleteInputRef}
+        />
         <input className="date" type="date" />
         <div className="with">with</div>
         <select className="family">
@@ -15,7 +61,14 @@ const BuildPlanMyself = () => {
           <option value="friend">Solo trip</option>
         </select>
         <input className="number-of-days" type="number" placeholder="Number of Days" />
-        <input className="to" type="text" placeholder="To" />
+        <input
+          className="to"
+          type="text"
+          placeholder="To"
+          value={placesTextTo}
+          onChange={handlePlacesTextChangeTo}
+          ref={autocompleteInputToRef}
+        />
       </div>
       <div className="lower-div">
         <label className="daywise-plan">Daywise Plan</label>
@@ -32,7 +85,9 @@ const BuildPlanMyself = () => {
           <option value="9">9</option>
           <option value="10">10</option>
         </select>
-        <img className="node-div-icon" alt="" src="/node-div.svg" />
+        <div className="node">
+          <div className="node-circle"></div>
+        </div>
       </div>
     </div>
   );
